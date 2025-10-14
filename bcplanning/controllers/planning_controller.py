084c9579b744e70047f86cf81f2fa314ac50c4cc
ahmadheriyanto <ahmadheriyanto@yahoo.com>
2025-10-14 08:26:32 +0700

    update 2025.10.14

--- bcplanning/controllers/planning_controller.py
+++ bcplanning/controllers/planning_controller.py
@@ -41,7 +41,7 @@ class PlanningApiController(http.Controller):
         return Response(json.dumps(vendor_recs),content_type='application/json;charset=utf-8',status=200)
 
     @http.route('/planning/contacts', type='http', auth='api_key', methods=['POST'], csrf=False)
-    def getpartners(self):    
+    def getcontacts(self):    
         """
         Body params:
         {
